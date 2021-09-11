72e5b0b6f972719cc421045fd01b320875b1ec59
Beeven Yip <beeven@hotmail.com>
2021-09-11 10:43:38 +0800

    Working in profile manager

--- Models/AriaProfile.cs
+++ Models/AriaProfile.cs
@@ -2,14 +2,15 @@ using System;
 
 namespace AriaBlazor.Models
 {
-    public class AriaProfile
+    public record AriaProfile
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = "Profile";
         public string Host { get; set; } = "localhost";
         public int Port { get; set; } = 6800;
         public string Path { get; set; } = "jsonrpc";
         public string Secret { get; set; } = "";
-        public Services.RPCProtocol Protocol { get; set; }
+        public Services.RPCProtocol Protocol { get; set; } = Services.RPCProtocol.WS;
+        public bool Expanded { get; set; } = false;
     }
 }
---
+++ Services/IProfileManager.cs
@@ -0,0 +1,14 @@
+﻿
+using AriaBlazor.Models;
+
+namespace AriaBlazor.Services;
+public interface IProfileManager
+{
+    IObservable<AriaProfile> ActiveProfileObservable {get;set;}
+
+    AriaProfile[] GetProfiles();
+
+    Task SaveProfilesAsync();
+    Task LoadProfilesAsync();
+
+}
